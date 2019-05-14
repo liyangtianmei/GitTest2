7e3c81c277e294cbd4f350a51c714238fd3b04e2
liyangtianmei <50508395+liyangtianmei@users.noreply.github.com>
2019-05-14 19:46:40 +0800

    remote update 01
    remote update 01

--- GitTest2/src/Git.java
+++ GitTest2/src/Git.java
@@ -3,5 +3,6 @@ public class Git {
 	public static void main(String args[]) {
 		System.out.println("eclipse test Git 01");
 		System.out.println("eclipse test Git 02");
+		System.out.println("eclipse test Git 03");
 	}
 }
