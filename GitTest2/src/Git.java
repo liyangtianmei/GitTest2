079845e49508f02382d09674be732293e8e82dcc
liyangtianmei <liyangtianmei@163.com>
2019-05-14 20:31:22 +0800

    eclipse test Git 06

--- GitTest2/src/Git.java
+++ GitTest2/src/Git.java
@@ -6,5 +6,7 @@ public class Git {
 		System.out.println("eclipse test Git 03");
 		System.out.println("remote test Git 04");
 		System.out.println("remote test Git 05");
+		System.out.println("remote test Git 06");
+
 	}
 }
